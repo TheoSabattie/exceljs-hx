d20778d3e640ddefdd3e020799493eb5f9b791c7
Théo Sabattié <t.sabattie@student.isartdigital.com>
2016-07-22 01:22:32 +0200

    Add ValueType

---
+++ src/ExcelValueType.hx
@@ -0,0 +1,16 @@
+package;
+
+/**
+ * ...
+ * @author Théo Sabattié
+ */
+@:jsRequire("exceljs", "ValueType")
+@:enum extern abstract ExcelValueType(Int) {
+    var Null;
+    var Merge;
+    var Number;
+    var String;
+    var Date;
+    var Hyperlink;
+    var Formula;
+}
