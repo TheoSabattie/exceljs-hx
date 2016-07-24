d62017a731663a818a007abd580419a6a2eeecbc
Théo Sabattié <t.sabattie@student.isartdigital.com>
2016-07-24 18:28:04 +0200

    Update Column

--- src/column/Column.hx
+++ src/column/Column.hx
@@ -11,12 +11,26 @@ extern class Column extends Style
 {
     private function new();
     
-    public var header:String;
+    public var header:Dynamic; // String or Array of String
 	public var key:String;
     public var width:Float;
     public var hidden:Bool;
+    public var number(default, never):Int;
+    public var worksheet(default, never):Sheet;
+    public var letter(default, never):String;
+    public var isCustomWidth(default, never):Bool;
+    public var defn:Dynamic;
+    public var headers(default, never):Array<Dynamic>;
+    public var headerCount(default, never):Int;
+    public var isDefault(default, never):Bool;
     
     @:overload(function(includer:Includer, callBack:Cell->Int->Void):Void{})
     public function eachCell(callBack:Cell->Int->Void):Void;
+    
+    public function toString():String;
+    public function equivalentTo(column:Column):Bool;
+    
+    public static function toModel(columns:Array<Column>):Array<Dynamic>; // ArrayModel
+    public static function fromModel(worksheet:Sheet, cols:Array<Dynamic>):Array<Column>; // cols -> ArrayModel
 }
 
